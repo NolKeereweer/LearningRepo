8decead8c1b6ecfaa6be7c4fe72e48f7a78f11ba
NolKeereweer <118395409+NolKeereweer@users.noreply.github.com>
2022-11-16 21:41:39 +0100

    initial commit of todo list app

---
+++ TodoList/Program.cs
@@ -0,0 +1,36 @@
+using TodoList;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var random = new Random();
+
+var tommy = new Hond();
+tommy.Naam = "Tommy";
+tommy.GewichtInKilo = 40;
+
+var zipper = new Hond();
+zipper.Naam = "Zipper";
+zipper.GewichtInKilo = 15;
+
+var mijnHonden = new List<Hond>();
+mijnHonden.Add(tommy);
+mijnHonden.Add(zipper);
+
+mijnHonden.Add(new Hond{ Naam = "Ficky", GewichtInKilo = 4});
+for (var i = 0; i < random.Next(4, 10); i++)
+{
+    mijnHonden.Add(new Hond{ Naam = "Mijn hond " + i, GewichtInKilo = 4 + i});
+}
+
+string result = "Mijn honden zijn: " + "\n";
+for (var i = 0; i < mijnHonden.Count; i++)
+{
+    result = result + mijnHonden[i].Naam + "\n";
+}
+    
+app.MapGet(pattern: "/", handler: () => result); 
+//app.MapGet(pattern: "/", handler: () => "mijn honden zijn: " + "\n" + tommy.Naam + "\n" + zipper.Naam + "\n" + mijnHonden.Last().Naam); 
+
+// app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoList;
+
+public class TodoItem
+{
+    
+}
+
+
+public class Hond
+{
+    public readonly int Poten = 4;
+    public string Naam;
+    public int GewichtInKilo;
+
+}
